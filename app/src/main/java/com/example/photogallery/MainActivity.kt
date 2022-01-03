3263e782566f1b3816eb3c1318422768d9bb6e26
klosik007 <pklos1992@gmail.com>
2022-01-03 23:47:38 +0100

    first sketch - toolbar and tabs

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.photogallery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tab: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tab = findViewById(R.id.tabHolder)
 
         supportFragmentManager
             .beginTransaction()
