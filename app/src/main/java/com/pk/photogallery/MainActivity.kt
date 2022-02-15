d622ceb674580620baa6fde49c9d0da9357fab9b
klosik007 <pklos1992@gmail.com>
2022-02-15 21:15:21 +0100

    MainActivity - minimal functionality

--- app/src/main/java/com/pk/photogallery/MainActivity.kt
+++ app/src/main/java/com/pk/photogallery/MainActivity.kt
@@ -2,20 +2,59 @@ package com.pk.photogallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.FrameLayout
+import androidx.core.view.marginTop
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
+import com.pk.photogallery.databinding.ActivityMainBinding
+import com.pk.photogallery.ui.Home
+import com.pk.photogallery.ui.MoreOptions
+import com.pk.photogallery.ui.Profile
+import com.pk.photogallery.ui.Search
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tab: TabLayout
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationBottom: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        tab = findViewById(R.id.tabHolder)
+        navigationBottom = binding.navigation
+        navigationBottom.setOnItemSelectedListener(bottomNavListener)
+
+        startSupportFragmentManager()
+    }
+
+    private fun startSupportFragmentManager(){
         supportFragmentManager
             .beginTransaction()
-            //.add(R.id.fragment_container, FreakyLauncherFragment())
+            .replace(R.id.view_pager, Home())
             .commit()
+    }
+
+    private val bottomNavListener = NavigationBarView.OnItemSelectedListener {
+        var fragment: Fragment? = null
+
+        when(it.itemId){
+            R.id.homeBtn -> fragment = Home()
+            R.id.searchBtn -> fragment = Search()
+            R.id.profileBtn -> fragment = Profile()
+            R.id.hamburgerBtn -> fragment = MoreOptions()
+        }
+
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.view_pager, fragment).commit()
+        }
+
+        true
     }
 }
