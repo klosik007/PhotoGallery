131d0cbfc9a020b4c45a4ad6e8e6d7397a466da6
klosik007 <pklos1992@gmail.com>
2022-02-15 21:53:19 +0100

    Profile minimal implementation

---
+++ app/src/main/java/com/pk/photogallery/ui/Profile.kt
@@ -0,0 +1,62 @@
+package com.pk.photogallery.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.tabs.TabLayout
+import com.pk.photogallery.R
+import com.pk.photogallery.databinding.ProfileFragmentBinding
+
+class Profile: Fragment() {
+    private lateinit var binding: ProfileFragmentBinding
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        tabLayout = binding.tabHolder
+        tabLayout.addOnTabSelectedListener(tabLayoutListener)
+
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private val tabLayoutListener = object : TabLayout.OnTabSelectedListener {
+
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            when(tab?.tag){
+                "PROFILE_TAB_PHOTOS" -> {
+
+                }
+
+                "PROFILE_TAB_FOLLOWERS" -> {
+
+                }
+
+                "PROFILE_TAB_FOLLOWING" -> {
+
+                }
+
+                "PROFILE_TAB_MOREINFO" -> {
+
+                }
+            }
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+            // Handle tab reselect
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+            // Handle tab unselect
+        }
+    }
+}
