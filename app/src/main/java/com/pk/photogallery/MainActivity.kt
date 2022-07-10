1ac8b7f45482817310b4c4fe31699bb7c474f336
klosik007 <pklos1992@gmail.com>
2022-07-10 20:32:36 +0200

    permissions sketches

--- app/src/main/java/com/pk/photogallery/MainActivity.kt
+++ app/src/main/java/com/pk/photogallery/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.pk.photogallery
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.FrameLayout
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
@@ -32,6 +39,17 @@ class MainActivity : AppCompatActivity() {
         navigationBottom.setOnItemSelectedListener(bottomNavListener)
 
         startSupportFragmentManager()
+        //requirePermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // TODO: make it useful
+        // https://developer.android.com/training/permissions/requesting
     }
 
     private fun startSupportFragmentManager(){
@@ -56,5 +74,54 @@ class MainActivity : AppCompatActivity() {
         }
 
         true
+    }
+
+    private fun requirePermissions(): Boolean {
+        val permissions = mutableListOf<String>()
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                ) {
+                    // TODO: educational UI: explain why are the permission is required
+                }
+            }
+
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                ) {
+                    // TODO: educational UI: explain why are the permission is required
+                }
+            }
+
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                permissions.add(Manifest.permission.CAMERA)
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.CAMERA)
+                ) {
+                    // TODO: educational UI: explain why are the permission is required
+                }
+            }
+        }
+
+        requestPermissions(permissions.toTypedArray(), 0) // TODO: requestCode 0?
+
+        return true
     }
 }
